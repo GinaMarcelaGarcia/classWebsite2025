d91137719663d448f7111f6fb56f0916162df799
GinaMarcelaGarcia <14492@ait.nsw.edu.au>
2025-04-23 01:25:30 +0000

    adding data

---
+++ .vscode/components/header.php
@@ -0,0 +1,44 @@
+<header class="main-header">
+        <a href="index.php">
+            <img class="logo" src="images/Logo-10.png">
+        </a>
+        <button type="button" class="menu-button">
+            <i class="fa-solid fa-bars"></i>
+        </button>
+        <form id="search">
+            <input type="search" name="search" placeholder="Search in our page">
+            <button type="submit" name="search-button">
+            <i class="fa-solid fa-magnifying-glass"></i>
+            </button>
+        </form>
+
+        <nav class="main-navigation"> 
+
+            <a href="index.php"> 
+                  <i class="fa-sharp-duotone fa-solid fa-house"></i> <br/>
+                <!-- <i class="fa-solid fa-house"></i> -->
+                Home
+            </a>
+         
+            <a href="products.php">
+                <i class="fa-sharp fa-solid fa-headphones"></i> <br/>
+                Products
+               
+            </a>
+        
+
+            <a href="about.php">
+                <i class="fa-sharp fa-solid fa-face-smile"></i> <br/>
+            About Us
+            </a>
+
+          
+            <a href="contact.php">
+            <i class="fa-sharp fa-solid fa-phone"></i> <br/>    
+            Contact
+            </a>
+
+
+        </nav>
+
+    </header>
---
+++ about.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+
+<?php
+//include head section
+include "components/head.php";
+?> 
+
+
+<?php
+// include header section
+include "components/header.php";
+?>
+
+<main>
+
+<h2>
+    <header class="second-header">
+    <a ="Main.html">Main </a> <br>
+    <a ="Equipment.html">Equipment</a>
+    <a ="Woman.html">Woman</a>
+    <a ="Men.html">Man</a>
+    <a ="Kids.html">Kids</a>
+    <a ="Discounts.html"> Discounts</a>
+</h2>
+
+<body>
+    <h1>About Us</h1>
+</body>
+</html>
+
+<title> OUR MISSION </title>
+<p> At ProGear Hub, our mission is to inspire and empower athletes of all levels to reach their fullest potential. We are dedicated to providing high-quality sports equipment, apparel, and expert advice to help you perform at your best. Whether you’re a beginner, a seasoned pro, or someone passionate about staying active, we believe in fostering a community where every athlete feels supported and motivated.
+
+</br>We strive to make sports accessible, enjoyable, and rewarding, promoting a healthy lifestyle and the values of teamwork, discipline, and perseverance. Our goal is not just to provide products, but to be a trusted partner in your journey towards success.
+    
+</br>Join us, and let's push the boundaries of what you can achieve, together.
+
+</p>
+
+<img
+class="photos-about"
+src="images/Fotos about us-19.jpg"
+size="10px"
+/> </br>
+
+<h2>What We Do</h2>
+
+Welcome to ProGear Hub
+
+We're passionate about the world of sports in all its forms. We're your go-to source for the latest news, results, analysis, and much more. Whether you're a fan of football, basketball, tennis, or extreme sports, you'll find up-to-date and relevant content covering all your sporting interests.
+
+Comprehensive Event Coverage
+
+From major international leagues to local tournaments, our coverage is thorough. We keep you informed with real-time results, match analysis, exclusive athlete interviews, and everything you need to stay close to your favorite teams and athletes.
+
+Expert Articles and Opinions
+
+We offer in-depth analysis of every sport, with insights from our experts so you don’t miss a single detail. From the latest strategies to the most accurate predictions, we have everything you need to fully understand every game and every championship.
+
+<img
+class="photos-about"
+src="images/Fotos about us-20.jpg"
+/> </br>
+
+</main>
+
+<?php
+    // include header section
+    include "components/footer.php";
+    ?>
+
+
+</body>
+
+</html>
